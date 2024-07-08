ed805c5bb5c7e667d37d188f80fbb249d709eee7
Danil Zaychenko <dan.zaychenko.2001@gmail.com>
2024-07-08 16:53:30 +0300

    fixes added swager

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('File Management API')
+    .setDescription('API documentation for file management system')
+    .setVersion('1.0')
+    .addBearerAuth() // If you're using JWT authentication
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
--- backend/src/typeorm.config.ts
+++ backend/src/typeorm.config.ts
@@ -1,3 +1,7 @@
+import { Permission } from '@app/entities/permission.entity';
+import { Folder } from '@app/entities/folder.entity';
+import { File } from '@app/entities/file.entity';
+import { User } from '@app/entities/user.entity';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
@@ -7,8 +11,8 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  //   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: process.env.NODE_ENV === 'development',
-  //   entities: [],
+  entities: [User, File, Folder, Permission],
   autoLoadEntities: true,
 };
