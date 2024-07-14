629e45ae35e524a93b90809fcb239d54009696aa
Danil Zaychenko <dan.zaychenko.2001@gmail.com>
2024-07-14 23:08:37 +0300

    app setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+
+import router from "./routers";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
