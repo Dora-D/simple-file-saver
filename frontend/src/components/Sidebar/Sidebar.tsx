5c32ea45b6ae8bdd813576d93b06ef3fd2bf0e87
Danil Zaychenko <dan.zaychenko.2001@gmail.com>
2024-07-16 00:25:13 +0300

    logic for work with main and available pages

--- frontend/src/components/Sidebar/Sidebar.tsx
+++ frontend/src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,27 @@
-import { Box, Stack, TextField, Typography } from "@mui/material";
-import { grey, green } from "@mui/material/colors";
+import { Chip, Stack, TextField, Typography } from "@mui/material";
+import { green, blueGrey } from "@mui/material/colors";
 import { useAppDispatch } from "../../hooks/reduxAppHooks";
 import debounce from "lodash.debounce";
-import { setSearchQuery } from "../../redux/store/slices/searchSlice";
+import {
+  setSearchIn,
+  setSearchQuery,
+} from "../../redux/store/slices/searchSlice";
 import CreateButton from "../CreateButton/CreateButton";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
   const dispatch = useAppDispatch();
+  const { pathname } = useLocation();
+  const isAvailable = pathname.includes("available-to-me");
+
+  useEffect(() => {
+    if (isAvailable) {
+      dispatch(setSearchIn("available"));
+    } else {
+      dispatch(setSearchIn("own"));
+    }
+  }, [isAvailable, dispatch]);
 
   const handleSearch = debounce(
     (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -22,7 +37,7 @@ const Sidebar = () => {
       padding={"40px 20px"}
       width={400}
       borderRight={"1px solid"}
-      borderColor={grey[500]}
+      borderColor={blueGrey[900]}
     >
       <Typography color={green[900]} variant="h4">
         VrealSoft Test Task
@@ -33,6 +48,24 @@ const Sidebar = () => {
         onChange={handleSearch}
         label="Search files and folders"
         variant="outlined"
+      />
+      <Chip
+        sx={{ fontSize: "16px", height: "40px" }}
+        component={Link}
+        clickable
+        to={"/drive"}
+        label="My Files"
+        variant="outlined"
+        color={!isAvailable ? "success" : "default"}
+      />
+      <Chip
+        sx={{ fontSize: "16px", height: "40px" }}
+        component={Link}
+        clickable
+        to={"/drive/available-to-me"}
+        label="Available to Me"
+        variant="outlined"
+        color={isAvailable ? "success" : "default"}
       />
     </Stack>
   );
