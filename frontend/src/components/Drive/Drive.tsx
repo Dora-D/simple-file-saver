be6af837c983d18ed05d46d9391d64fc1c15b3ba
Danil Zaychenko <dan.zaychenko.2001@gmail.com>
2024-07-16 00:23:12 +0300

    clean code in main component

--- frontend/src/components/Drive/Drive.tsx
+++ frontend/src/components/Drive/Drive.tsx
@@ -1,43 +1,13 @@
-import React, { useState } from "react";
 import Layout from "../Layout/Layout";
-import { Box, Breadcrumbs, Link, Stack, Typography } from "@mui/material";
-import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import { useSearchQuery } from "../../services/fileManagerApi";
-import { useAppSelector } from "../../hooks/reduxAppHooks";
-import { Link as RouterLink, useLocation } from "react-router-dom";
-import { blueGrey } from "@mui/material/colors";
+import { Stack } from "@mui/material";
 import DriveTable from "../DriveTable/DriveTable";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
 
 const Drive = () => {
-  const location = useLocation();
-
-  const [breadcrumbs, setBreadcrumbs] = useState([
-    <Link
-      key={"drive"}
-      underline="none"
-      color={blueGrey[900]}
-      component={RouterLink}
-      to={"/drive"}
-    >
-      <Typography variant="h5">My Drive</Typography>
-    </Link>,
-  ]);
-
-  const { searchIn, query } = useAppSelector(({ search }) => search);
-
-  const { data } = useSearchQuery({ query, searchIn });
-
-  console.log(data);
-
   return (
     <Layout>
       <Stack spacing={2} padding={"40px 20px"}>
-        <Breadcrumbs
-          separator={<NavigateNextIcon fontSize="small" />}
-          aria-label="breadcrumb"
-        >
-          {breadcrumbs}
-        </Breadcrumbs>
+        <Breadcrumbs />
         <DriveTable />
       </Stack>
     </Layout>
