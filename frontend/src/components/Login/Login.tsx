d4f3f85613e2f81a933ed009df61f76bab9b3dcf
Danil Zaychenko <dan.zaychenko.2001@gmail.com>
2024-07-16 09:20:02 +0300

    login page changes

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,7 +1,8 @@
-import { Box, Container } from "@mui/material";
+import { Box, Container, Stack, Typography } from "@mui/material";
 import { GoogleLogin } from "@react-oauth/google";
 import { useAuth } from "../../hooks/useAuth";
 import { useAppSelector } from "../../hooks/reduxAppHooks";
+import { blueGrey, green } from "@mui/material/colors";
 
 const Login = () => {
   const { login, navigateToMainPage } = useAuth();
@@ -21,24 +22,38 @@ const Login = () => {
         height: "100vh",
       }}
     >
-      <Box
+      <Stack
         width={300}
-        height={200}
         border={"1px solid"}
         borderRadius="6px"
-        borderColor={"primary.main"}
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
+        borderColor={blueGrey[100]}
+        spacing={2}
       >
-        <GoogleLogin
-          onSuccess={login}
-          onError={() => {
-            console.log("Login Failed");
-          }}
-          useOneTap
-        />
-      </Box>
+        <Stack
+          direction={"row"}
+          borderBottom={"1px solid"}
+          padding={"10px 0"}
+          borderColor={blueGrey[100]}
+          alignItems={"center"}
+          justifyContent={"center"}
+        >
+          <Typography variant="h5" color={green[900]}>
+            VrealSoft test task
+          </Typography>
+        </Stack>
+        <Stack alignItems={"center"} spacing={3} pb={"40px"}>
+          <Typography variant="body1" color={blueGrey[900]}>
+            SignIn/SingUp
+          </Typography>
+          <GoogleLogin
+            onSuccess={login}
+            onError={() => {
+              console.log("Login Failed");
+            }}
+            useOneTap
+          />
+        </Stack>
+      </Stack>
     </Container>
   );
 };
